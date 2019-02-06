d2e15bffc567e52b58967b797c03c126d07ad73d
Justin <jwhittington92@yahoo.com>
2019-02-06 07:07:20 -0500

    loading component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import CurrentWeather from "../CurrentWeather";
 import WeatherResults from "../WeatherResults";
+import Loading from "../Loading";
 import Api from "../../utils/Api";
 import "./Home.css";
 
@@ -11,8 +12,14 @@ export default class Home extends React.Component {
   state = {
     location: "",
     result: [],
-    objCheck: false
+    objCheck: false,
+    load: false,
+    check: false
   };
+
+  // componentDidMount() {
+  //   setTimeout(() => this.setState({ check: true }), 1500);
+  // }
 
   // Function to check if result object is empty
   isEmpty = obj => {
@@ -37,6 +44,7 @@ export default class Home extends React.Component {
   // Function for getting data from the API and then checking for an empty object
   handleSubmit = event => {
     event.preventDefault();
+    this.setState({ load: true });
 
     Api.getFiveDay(this.state.location)
       .then(res => {
@@ -49,27 +57,31 @@ export default class Home extends React.Component {
 
     // If data is found in this.state.result, objCheck switched to true for conditional render in HTML
     if (this.isEmpty(this.state.result)) {
-      this.setState(() => {
+      setTimeout(() => this.setState(() => {
         return {
+          load: false,
           objCheck: true
         };
-      });
-      checker = true;
+      }), 1500);
+      
+      // checker = true;
     }
   };
 
   render() {
     // Conditional rendering based on the object check function
-    if (checker) {
+    if (this.state.objCheck) {
       return (
         <div id="height100">
           <WeatherResults result={this.state.result} />
         </div>
       );
+    } else if (this.state.load) {
+      return <Loading />;
     } else {
       return (
         <CurrentWeather
-        id="height100"
+          id="height100"
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
